86805a96c94804312af899def756c2214aec54a8
zaki <zakiyudhistira.itb69@gmail.com>
2024-03-12 20:40:51 +0700

    add : few functions

--- src/BezierCurve.py
+++ src/BezierCurve.py
@@ -11,21 +11,29 @@ class BezierCurve:
     def add(self, point : Point):
         self.count += 1
         self.points.append(point)
-    def createCruve(self, brute : bool):
+    def clearResults(self):
+        self.res
+    def createCurve(self, brute : bool):
         if brute:
             increment = 1 / (2**self.nIteration)
             t = float(0)
-            P1 = self.points[0]
-            P2 = self.points[1]
-            P3 = self.points[2]
+            p0 = self.points[0]
+            p1 = self.points[1]
+            p2 = self.points[2]
             while(t <= 1):
-                Q1 = P1*(1-t) + P2*t
-                Q2 = P2*(1-t) + P3*t
-                R1 = (1-t)*Q1 + Q2*t
-                self.results.append(R1)
+                q0 = p0*(1-t) + p1*t
+                q1 = p1*(1-t) + p2*t
+                r0 = (1-t)*q0 + q1*t
+                self.results.append(r0)
                 t += increment
             else:
                 print("ganteng")
+    def createCurveDnc(self, Iterations : int, p0 : Point, p1 : Point, p2 : Point):
+        if Iterations == 0:
+        else:
+            
+
+
     def printPoints(self):
         for x in self.points:
             x.printCoordinate()
@@ -37,5 +45,5 @@ main = BezierCurve(2)
 main.add(Point.Point(2,2))
 main.add(Point.Point(6,5))
 main.add(Point.Point(9,3))
-main.createCruve(True)
+main.createCurve(True)
 main.printResult()
