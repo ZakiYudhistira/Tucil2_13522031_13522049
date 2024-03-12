611d7f32172d89d6d770b78f1f005fbbd1f89e01
VansonK <vansonkurnialim@gmail.com>
2024-03-12 18:41:01 +0700

    display

--- src/BezierCurve.py
+++ src/BezierCurve.py
@@ -1,3 +1,8 @@
+class Point :
+    def __init__(self, x, y) -> None:
+        self.x = x 
+        self.y = y
+
 class BezierCurve:
     def __init__(self) -> None:
         self.points = []
--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
 import BezierCurve as Bc
 import Display as Dis
+
+A = Bc.BezierCurve()
+
+P0 = Bc.Point(0,0)
+P1 = Bc.Point(1,1)
+P2 = Bc.Point(2,0)
+
+A.points.append(P0)
+A.points.append(P1)
+A.points.append(P2)
+
+print(A.points[0].x)
