6d2673f92e6b50cdc00670c85766f883592e9b32
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2024-03-13 15:35:57 +0700

    add : n points for bruteforce algo

--- src/BezierCurve.py
+++ src/BezierCurve.py
@@ -3,63 +3,78 @@ import numpy as np
 import Display
 import matplotlib.pyplot as plt
 import time
+import math as mt
 
 class BezierCurve:
     def __init__(self, Iterate) -> None:
-        self.points = []
+        # Attributes assignment
+        self.points = [] # Anchor points
         self.count = 0
         self.nIteration = Iterate
-        self.results = []
+        self.results = [] # Result points
 
     def add(self, point : Point):
+        """
+        Add a point into the anchor points array
+        """
         self.count += 1
         self.points.append(point)
 
     def addSol(self, point : Point):
+        """
+        Add a point into the result array
+        """
         self.results.append(point)
 
     def clearResults(self):
-        self.res
+        """
+        Delete result points
+        """
+        self.results = []
 
     def createCurve(self, brute : bool):
+        """
+        Bezier curve generation algorithm
+        """
+        print(f"Depth : {self.nIteration}")
         if brute:
+            # Using the bruteforce approach
             start = time.time()
-            print("Brute Force")
+            print(">> Using the BRUTEFORCE approach <<")
             increment = 1 / (2**self.nIteration)
+            # n depth increment
             t = float(0)
-            p0 = self.points[0]
-            p1 = self.points[1]
-            p2 = self.points[2]
             while(t <= 1):
-                q0 = p0*(1-t) + p1*t
-                q1 = p1*(1-t) + p2*t
-                r0 = (1-t)*q0 + q1*t
-                self.results.append(r0)
+                temp = Point.Point(0,0)
+                n = len(self.points) - 1
+                for i in range(len(self.points)):
+                    # Bezier sum formula
+                    temp += mt.comb(n,i)*((1-t)**(n-i))*(t**i)*self.points[i]
                 t += increment
+                self.addSol(temp)
+                # bezier point addition to the result array
             end = time.time()
-            Display.plotDot(self.points[0].x, self.points[0].y)
-            Display.plotDot(self.points[1].x, self.points[1].y)
-            Display.plotDot(self.points[2].x, self.points[2].y)
-            Display.plotLine(main.points)
-            Display.plotLine(main.results)
-            plt.show()
+            print(">> BRUTEFORCE Runtime : ", end="")
             print((end - start) * 1000, "ms")
         else:
+            # Using the divide and conquer approach
             start = time.time()
-            print("Divide and Conquer")
+            print(">> Using the DIVIDE AND CONQUER approach <<")
             self.addSol(self.points[0])
-            self.createCurveDnc(self.nIteration, self.points[0], self.points[1], self.points[2])
-            self.addSol(self.points[2])
+            # first anchor point addition
+            for i in range(len(self.points)-2):
+                self.createCurveDnc(self.nIteration, self.points[0+i], self.points[1+i], self.points[2+i])
+            self.addSol(self.points[len(self.points)-1])
             end = time.time()
-            Display.plotDot(self.points[0].x, self.points[0].y)
-            Display.plotDot(self.points[1].x, self.points[1].y)
-            Display.plotDot(self.points[2].x, self.points[2].y)
-            Display.plotLine(main.points)
-            Display.plotLine(main.results)
-            plt.show()
+            print(">> DIVIDE AND CONQUER Runtime : ", end="")
             print((end - start) * 1000, "ms")
+        print(">> Displaying Curve... <<")
+        return end - start
 
     def createCurveDnc(self, Iterations : int, p0 : Point, p1 : Point, p2 : Point):
+        """
+        Using the divide and conquer approach in the bezier curve generation
+        """
         if Iterations == 0:
             pass
         else:
@@ -71,18 +86,35 @@ class BezierCurve:
             self.createCurveDnc(Iterations-1, midPoint2, midPoint1, p2)
 
     def printPoints(self):
+        """
+        To print each anchor point coordinate in the bezier curve
+        """
         for x in self.points:
             x.printCoordinate()
 
     def printResult(self):
+        """
+        To print each result point coordinate in the bezier curve
+        """
         for x in self.results:
             x.printCoordinate()
 
-main = BezierCurve(2)
+    def displayCurve(self):
+        """
+        Bezier curve render
+        """
+        for point in self.points:
+            Display.plotDot(point.x, point.y)
+        Display.plotLine(self.points)
+        Display.plotLine(self.results)
+        plt.show()
+
+main = BezierCurve(15)
+main.add(Point.Point(-2,-8))
 main.add(Point.Point(2,2))
 main.add(Point.Point(6,5))
-main.add(Point.Point(9,3))
-main.createCurve(False)
+main.createCurve(True)
+main.displayCurve()
 # main.printResult()
 
 
